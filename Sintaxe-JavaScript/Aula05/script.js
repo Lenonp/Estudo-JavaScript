02dd5208987c4fe920f2470dcb1b6e0eb2c6ba14
ELITO LENON PRATES <94411573+Lenonp@users.noreply.github.com>
2022-07-18 14:03:45 -0300

    aula-05 Commit

---
+++ Sintaxe-JavaScript/Aula05/script.js
@@ -0,0 +1,19 @@
+// funções - functions
+
+// como declarar
+// deve ser declarada usando a palavra function seguida de "()" e "{}"
+/* function funcao() {
+    console.log('mensagem de uma função');
+}
+// chamando a função
+funcao(); */
+
+ 
+// função com parâmetros
+// definindo parâmetros
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+// chamando a função atribuindo valor aos parâmetros
+mensagem('Ola mundo', 'Mundo!'); 
