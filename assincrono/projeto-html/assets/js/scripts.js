968a665de6a633e54fc0dbb982ff07cc4b029851
ELITO LENON PRATES <94411573+Lenonp@users.noreply.github.com>
2022-07-22 10:36:43 -0300

    assin commit

---
+++ assincrono/projeto-html/assets/js/scripts.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+	    return json.webpurl;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+const loadImg = async () => {
+	const catImg = document.getElementsByTagName('Cat');
+	catImg.src = await getCats();
+};
+
+catBtn.addEventListener('click', loadImg);
+
+loadImg();
